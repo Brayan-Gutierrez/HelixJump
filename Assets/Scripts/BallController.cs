73324cb408aa9e7113518b7e16b463c2376f7169
Brayan Gutiérrez <br.desarrollador@gmail.com>
2023-06-04 16:27:42 -0600

    Add script to the ball and to the camera
    Ball bounce and the camera follows the ball.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float impulseForce = 3f;
+    private bool ignoreNextcollision;
+
+    /*We check that no double force is applied to the ball when
+     *touches two propellers
+     */
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (ignoreNextcollision)
+        {
+            return;
+        }
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.up * impulseForce, ForceMode.Impulse);
+
+        ignoreNextcollision = true;
+        Invoke("AllowNextCollision", 0.2f);
+    }
+
+    private void AllowNextCollision()
+    {
+        ignoreNextcollision = false;
+    }
+}
